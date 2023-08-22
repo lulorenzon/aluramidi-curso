7f2bf79aa5889ba15ed982b5f6753b0c62bdfb90
luana.santos <luana.santos@g.globo>
2023-08-22 19:11:58 -0300

    feat: init javascripit

--- main.js
+++ main.js
@@ -1,39 +1,56 @@
-function tocaSom (seletorAudio) {
-    const elemento = document.querySelector(seletorAudio);
+// function tocaSom (seletorAudio) {
+//     const elemento = document.querySelector(seletorAudio);
 
-    if (elemento && elemento.localName === 'audio') {
-        elemento.play();
-    }
-    else {
-        //alert('Elemento não encontrado');
-        console.log('Elemento não encontrado ou seletor inválido');
-    }
+//     if (elemento && elemento.localName === 'audio') {
+//         elemento.play();
+//     }
+//     else {
+//         //alert('Elemento não encontrado');
+//         console.log('Elemento não encontrado ou seletor inválido');
+//     }
 
-}
+// }
 
-const listaDeTeclas = document.querySelectorAll('.tecla');
+// const listaDeTeclas = document.querySelectorAll('.tecla');
 
-//para
-for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+// //para
+// for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
-    const tecla = listaDeTeclas[contador];
-    const instrumento = tecla.classList[1];
-    const idAudio = `#som_${instrumento}`; //template string
+//     const tecla = listaDeTeclas[contador];
+//     const instrumento = tecla.classList[1];
+//     const idAudio = `#som_${instrumento}`; //template string
 
-    tecla.onclick = function () {
-        tocaSom(idAudio);
-    }
+//     tecla.onclick = function () {
+//         tocaSom(idAudio);
+//     }
 
-    tecla.onkeydown = function (evento) {
+//     tecla.onkeydown = function (evento) {
 
-        if (evento.code === 'Space' || evento.code === 'Enter') {
-            tecla.classList.add('ativa');
-        }
+//         if (evento.code === 'Space' || evento.code === 'Enter') {
+//             tecla.classList.add('ativa');
+//         }
 
-    }
+//     }
 
-    tecla.onkeyup = function () {
-        tecla.classList.remove('ativa');
-    }
+//     tecla.onkeyup = function () {
+//         tecla.classList.remove('ativa');
+//     }
 
-}
+// }
+
+
+
+palavraPalindromo()
+
+function  palavraPalindromo () {
+  const palavra = "rever";
+  const separandoPalavra = palavra.split ("");
+  const palavraInvertida = separandoPalavra.reverse()
+  palavraInvertida = palavraInvertida,join("");
+
+  if (palavra == palavraInvertida){
+    console.log("palavra invertida" + palavra + "é um polindromo");
+  } else {
+    console.log("palavra invertida" + palavra + "é um polindromo");
+  }
+  }
---
+++ polidromo.js
@@ -0,0 +1,35 @@
+palavraPalindromo()
+
+function  palavraPalindromo () {
+  var palavra = "rever";
+  var separandoPalavra = palavra.split ("");
+  var palavraInvertida = separandoPalavra.reverse();
+  palavraInvertida = palavraInvertida.join("");
+
+  if (palavra == palavraInvertida){
+    console.log("palavra invertida " + palavra + " é um polindromo");
+  }
+    
+  else {
+    console.log("palavra invertida " + palavra + " nao é um polindromo");
+  }
+  
+}
+
+
+const essaPalavraPalindromo = function () {
+  var palavra = "subinoonibus";
+  var separandoPalavra = palavra.split ("");
+  var palavraInvertida = separandoPalavra.reverse();
+  palavraInvertida = palavraInvertida.join("");
+
+  if (palavra == palavraInvertida){
+    console.log("palavra invertida " + palavra + " é um polindromo");
+  }
+    
+  else {
+    console.log("palavra invertida " + palavra + " nao é um polindromo");
+  }
+  
+}
+essaPalavraPalindromo()
